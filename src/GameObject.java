e1e04c17040bd5c29e23f806aa3f69a782991fdf
leafcabral <rafaelcabral4100@gmail.com>
2025-10-08 19:19:04 -0300

    Improved Vec2D methods and finished GameObject abstract class

--- src/GameObject.java
+++ src/GameObject.java
@@ -5,26 +5,44 @@ import java.awt.image.BufferedImage;
 public abstract class GameObject {
 	public Vec2D pos;
 	public Vec2D size;
-	public Vec2D speed;
+	public Vec2D direction;
+	public int speed;
 	
 	private BufferedImage sprite;
 	private Color fallback_color;
 	
-	public GameObject(Vec2D pos, Vec2D size, Vec2D speed,
+	
+	public GameObject(Vec2D pos, Vec2D size,
+		          Vec2D direction, int speed,
 			  BufferedImage sprite, Color fallback_color) {
 		this.pos = new Vec2D(pos);
 		this.size = new Vec2D(size);
-		this.speed = new Vec2D(speed);
+		this.direction = new Vec2D(direction);
+		this.speed = speed;
 		
 		this.sprite = sprite;
 		this.fallback_color = fallback_color;
 	}
 	
-	public abstract void update();
+	
+	public abstract void update(double delta);
 	public abstract void draw(Graphics2D g2);
+	
 	
 	public void draw_fallback(Graphics2D g2) {
 		g2.setColor(fallback_color);
 		g2.fillRect(pos.x, pos.y, size.x, size.y);
 	}
+	
+	public Vec2D getCenter() {
+		Vec2D center = new Vec2D();
+		center.x = pos.x + (size.x * 2);
+		center.y = pos.y + (size.y * 2);
+		
+		return center;
+	}
+	
+	public Vec2D getVelocity() {
+		return direction.normalize().multiply(speed);
+	}	
 }
--- src/Vec2D.java
+++ src/Vec2D.java
@@ -1,13 +1,46 @@
 public class Vec2D {
 	public int x, y;
 	
+	
+	public Vec2D() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
 	public Vec2D(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
+	
 	public Vec2D(Vec2D other) {
 		this.x = other.x;
 		this.y = other.y;
+	}
+	
+	
+	public Vec2D add(int value) {
+		return new Vec2D(this.x + value, this.y + value);
+	}
+	public Vec2D add(Vec2D b) {
+		return new Vec2D(this.x + b.x, this.y + b.y);
+	}
+	
+	public void addIp(Vec2D b) {
+		this.x += b.x;
+		this.y += b.y;
+	}
+	public void addIp(int value) {
+		this.x += value;
+		this.y += value;
+	}
+	
+	public Vec2D multiply(int value) {
+		return new Vec2D(this.x * value, this.y * value);
+	}
+	
+	public void multiplyIp(int value) {
+		this.x *= value;
+		this.y *= value;
 	}
 	
 	public double distance(Vec2D b) {
@@ -15,5 +48,25 @@ public class Vec2D {
 		int deltaY = b.y - this.y;
 		
 		return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+	}
+	
+	public double magnitude() {
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	}
+	
+	public Vec2D normalize() {
+		double magnitude = this.magnitude();
+		Vec2D normalized = new Vec2D();
+		
+		normalized.x = (int) (((double) this.x) / magnitude);
+		normalized.y = (int) (((double) this.y) / magnitude);
+		
+		return normalized;
+	}
+	public void normalizeIP() {
+		double magnitude = this.magnitude();
+		
+		this.x = (int) (((double) this.x) / magnitude);
+		this.y = (int) (((double) this.y) / magnitude);
 	}
 }
