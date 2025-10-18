5962dbf1d8aa042ebf0c3b3f33aa16e3ee74ad38
leafcabral <rafaelcabral4100@gmail.com>
2025-10-17 21:58:03 -0300

    Implemented straight and wave movement for enemy

--- src/game/entities/Enemy.java
+++ src/game/entities/Enemy.java
@@ -1,5 +1,6 @@
 package game.entities;
 
+import game.entities.patterns.MovementPattern;
 import game.utils.Vec2D;
 
 import java.awt.Color;
@@ -8,27 +9,30 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public class Enemy extends GameObject {
+	private MovementPattern movement;
 	public int life;
 	private float iFrameSeconds = 0;
 
 	public Enemy(Vec2D pos, Vec2D size,
 	              Vec2D direction, int speed,
 	              BufferedImage sprite, Color fallback_color,
-		      int life) {
+		      int life, MovementPattern movement) {
 		super(pos, size, direction, speed, sprite, fallback_color);
 		this.life = life;
+		this.movement = movement;
 	}
-	public Enemy(Vec2D pos, BufferedImage img) {
+	public Enemy(Vec2D pos, BufferedImage img, MovementPattern movement) {
 		this(
 			pos, new Vec2D(75, 75),
 			new Vec2D(0, 1), 200,
 			img, Color.RED,
-			1
+			1, movement
 		);
 	}
 
 	@Override
 	public void update(float delta) {
+		this.direction = movement.update(delta, this.pos);
 		super.move(delta);
 		
 		if (iFrameSeconds > 0) {
