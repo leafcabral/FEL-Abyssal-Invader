b6f6d2026d4db7f1339c98ede6f5b39722473898
leafcabral <rafaelcabral4100@gmail.com>
2025-10-18 11:27:19 -0300

    Fixed entities, I think

--- src/game/entities/Bullet.java
+++ src/game/entities/Bullet.java
@@ -7,54 +7,24 @@ import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
-public class Bullet extends GameObject{
-	public Bullet(Vec2D pos, Vec2D size,
-	              Vec2D direction, int speed,
-	              BufferedImage sprite, Color fallback_color) {
-		super(pos, size, direction, speed, sprite, fallback_color);
+public class Bullet extends Entity {
+	
+	public Bullet(
+			Vec2D pos,
+			Vec2D direction, float speed,
+			Vec2D size,
+			BufferedImage sprite, Color color,
+			int pierceCount) {
+		super(pos, direction, speed, size, sprite, size, color, pierceCount);
 	}
 
-	@Override
-	public void update(float delta) {
-		super.move(delta);
-	}
-	
-	@Override
-	public void draw(Graphics2D g2) {
-		if (sprite == null) {
-			draw_fallback(g2);
-		} else {
-			// Função que pega Vec2D e acha seu angulo
-			double angle = Math.atan2(direction.y, direction.x);
-			
-			AffineTransform original = g2.getTransform();
-			int centerX = (int)(pos.x + size.x / 2);
-			int centerY = (int)(pos.y + size.y / 2);
-			
-			// Move centro da tela para centro da bala
-			g2.translate(centerX, centerY);
-			// Roda a tela (não tem como rodar o sprite)
-			g2.rotate(angle);
-			// Volta pra posição inicial
-			g2.translate(-size.x / 2, -size.y / 2);
-			
-			
-			g2.drawImage(
-				sprite,
-				0, 0,
-				(int) size.x, (int) size.y,
-				null
-			);
-			
-			g2.setTransform(original);
-		}
-		
-	}
 	public static Bullet newDefaultBullet(Vec2D pos, BufferedImage img) {
 		return new Bullet(
-			pos, new Vec2D(30, 30),
+			pos,
 			new Vec2D(0, -1), 1000,
-			img, Color.YELLOW
+			new Vec2D(30, 30),
+			img, Color.YELLOW,
+			1
 		);
 	}
 	
@@ -68,9 +38,11 @@ public class Bullet extends GameObject{
 		
 		for (int i = 0; i < bullets.length; i++) {
 			bullets[i] = new Bullet(
-				pos, new Vec2D(30, 30),
-				directions[i], 1000,
-				img, Color.GREEN
+				pos,
+				new Vec2D(0, -1), 1000,
+				new Vec2D(30, 30),
+				img, Color.RED,
+				1
 			);
 		}
 		
@@ -79,9 +51,12 @@ public class Bullet extends GameObject{
 	
 	public static Bullet newBlastBullet(Vec2D pos, BufferedImage img) {
 		return new Bullet(
-			pos.add(-50), new Vec2D(90, 90),
+			pos,
 			new Vec2D(0, -1), 700,
-			img, Color.BLUE
+			new Vec2D(90, 90),
+			img, Color.BLUE,
+			3
 		);
+		
 	}
 }
--- src/game/entities/Player.java
+++ src/game/entities/Player.java
@@ -1,58 +1,33 @@
 package game.entities;
 
 import game.utils.Vec2D;
-
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage; // Importa para trabalhar com imagens.
+import java.awt.image.BufferedImage;
 
 
-public class Player extends GameObject {
+public class Player extends Entity {
 	public enum WeaponType {
 		DEFAULT, SHOTGUN, BLAST
 	}
-	
-	private int life;
-	private int maxLife;
-	
 	private WeaponType currentWeapon;
 	private float weaponDelays[] = {0.5f, 1.0f, 2.0f};
 	private float weaponTimers[] = {0, 0, 0};
-	
-	private float iFrameSeconds = 0;
 	
 	private BufferedImage sprites[];
 	private int imgIndex = 0;
 	private final float changeSpriteDelay = 0.1f;
 	private float changeSpriteTimer = changeSpriteDelay;
 
-	public Player(Vec2D pos, Vec2D size,
-	              Vec2D direction, int speed,
-	              BufferedImage sprite, Color fallback_color,
-		      int life) {
-		super(pos, size, direction, speed, sprite, fallback_color);
-		this.life = life;
-		this.maxLife = life;
-		this.switchWeapon(WeaponType.DEFAULT);
-	}
-	public Player(
-			Vec2D pos, BufferedImage img1,
-			BufferedImage img2, BufferedImage img3) {
-		this(
-			pos, new Vec2D(75, 75),
-			new Vec2D(0, -1), 500, 
-			img1, Color.GREEN,
-			3
-		);
-		
+	public Player(Vec2D pos, BufferedImage img1, BufferedImage img2, BufferedImage img3) {
+		super(pos, new Vec2D(75, 75), img1, Color.BLUE, 3);
 		this.sprites = new BufferedImage[]{img1, img2, img3};
+		this.movement.speed = 500;
+		this.switchWeapon(WeaponType.DEFAULT);
 	}
 
 	@Override
-	public void update(float delta) {
-		if (iFrameSeconds > 0) {
-			iFrameSeconds -= delta;
-		}
+	public void _process(float delta) {
+		super._process(delta);
 
 		for (int i = 0; i < weaponTimers.length; i++) {
 			if (weaponTimers[i] > 0) {
@@ -64,45 +39,28 @@ public class Player extends GameObject {
 			if (changeSpriteTimer > 0) {
 				changeSpriteTimer -= delta;
 			} else {
-				if (imgIndex != sprites.length - 1) {
-					imgIndex++;
-				}
+				imgIndex = (imgIndex + 1) % sprites.length;
+				sprite.texture = sprites[imgIndex];
 				changeSpriteTimer = changeSpriteDelay;
 			}
 		} else {
-			changeSpriteTimer = changeSpriteDelay;
 			imgIndex = 0;
+			sprite.texture = sprites[imgIndex];
 		}
 	};
 	
-	@Override
-	public void draw(Graphics2D g2) {
-		if (sprite == null) {
-			draw_fallback(g2);
-		} else if (this.direction.x >= 0) {
-			g2.drawImage(
-				sprite,
-				(int) pos.x, (int) pos.y,
-				(int) size.x, (int) size.y,
-				null
-			);
-		} else {
-			g2.drawImage(
-				sprite,
-				(int) pos.x + (int)size.x, (int) pos.y,
-				(int) size.x * -1, (int) size.y,
-				null
-			);
-		}
+	public void switchWeapon(WeaponType newWeapon) {
+		this.currentWeapon = newWeapon;
+	}
+	public WeaponType getCurrentWeapon() {
+		return currentWeapon;
 	}
 	
-	public void move(float velocity) {
-		this.pos.x += velocity;
-		this.sprite = this.sprites[imgIndex];
+	public void setWeaponTimer(float newTime) {
+		weaponTimers[currentWeapon.ordinal()] = newTime;
 	}
-	
-	public void makeInvencible(float seconds) {
-		iFrameSeconds = seconds;
+	public float getWeaponTimer() {
+		return weaponTimers[currentWeapon.ordinal()];
 	}
 	
 	public Boolean canShoot() {
@@ -116,57 +74,13 @@ public class Player extends GameObject {
 		setWeaponTimer(0);
 	}
 	
-	public int getCurrentLife() {
-		return life;
-	}
-	
-	public int getMaxLife() {
-		return maxLife;
-	}
-
-	public boolean takeDamage() {
-		if (this.life > 1 && this.iFrameSeconds <= 0) {
-			this.life--;
-			makeInvencible(1);
-			return false;
-		} else if (this.life == 1) {
-			this.life--;
-			return true;
-		}
-
-		return false;
-	}
-
-	public void heal(int lifeToHeal) {
-		this.life += lifeToHeal;
-	}
-
-	public void resetLife() {
-		this.life = 3;
-	}
-	
-	public void setWeaponTimer(float newTime) {
-		weaponTimers[currentWeapon.ordinal()] = newTime;
-	}
-	public float getWeaponTimer() {
-		return weaponTimers[currentWeapon.ordinal()];
-	}
-	
-	public void switchWeapon(WeaponType newWeapon) {
-		this.currentWeapon = newWeapon;
-	}
-	
-	public WeaponType getCurrentWeapon() {
-		return currentWeapon;
-	}
-	    
 	public float getWeaponCooldown(WeaponType weapon) {
 		return weaponTimers[weapon.ordinal()];
 	}
 
 	public float getWeaponCooldownProgress(WeaponType weapon) {
 		int index = weapon.ordinal();
-		if (weaponTimers[index] <= 0) return 0f;
+		if (weaponTimers[index] <= 0) { return 0f; }
 		return weaponTimers[index] / weaponDelays[index];
 	}
 }
