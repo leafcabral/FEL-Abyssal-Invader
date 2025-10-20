e0061704509e482440bc6ce21c41b24124bc0640
leafcabral <rafaelcabral4100@gmail.com>
2025-10-19 23:00:50 -0300

    feat: Added default and shooter attack pattern

--- src/game/entities/Enemy.java
+++ src/game/entities/Enemy.java
@@ -1,5 +1,6 @@
 package game.entities;
 
+import game.entities.patterns.AttackPattern;
 import game.entities.patterns.MovementPattern;
 import game.utils.Vec2D;
 
@@ -8,21 +9,39 @@ import java.awt.image.BufferedImage;
 
 public class Enemy extends Entity {
 	private MovementPattern movementPattern;
-	private float iFrameSeconds = 0;
+	private AttackPattern attackPattern;
+	private EnemyType type;
+	
+	public enum EnemyType {
+		STRAIGHT_NONE(0),
+		WAVE_NONE(1),
+		STRAIGHT_SHOOT(2),
+		SIDE_SHOOT(3);
+		
+		public final int difficulty;
+		EnemyType(int difficulty) { this.difficulty = difficulty; }
+	}
 
 	public Enemy(Vec2D pos, Vec2D size,
-	              Vec2D direction, int speed,
-	              BufferedImage sprite, Color fallback_color,
-		      int life, MovementPattern movement) {
+			Vec2D direction, int speed,
+			BufferedImage sprite, Color fallback_color,
+			 int life, MovementPattern movement, AttackPattern attack,
+			 EnemyType type) {
 		super(pos, size, direction, speed, sprite, fallback_color, life);
 		this.movementPattern = movement;
+		this.attackPattern = attack;
+		this.type = type;
+		
 	}
-	public Enemy(Vec2D pos, BufferedImage img, MovementPattern movement) {
+	public Enemy(Vec2D pos, BufferedImage img, EnemyType type, Vec2D screenSize) {
 		this(
 			pos, new Vec2D(75, 75),
 			new Vec2D(1, 0), 200,
 			img, Color.RED,
-			1, movement
+			getLifeForType(type),
+			getMovementPatternForType(type, screenSize, new Vec2D(img.getWidth(), img.getHeight())),
+			getAttackPatternForType(type),
+			type
 		);
 	}
 
@@ -38,5 +57,45 @@ public class Enemy extends Entity {
 	
 	public void makeIvencible(float seconds) {
 		iFrameSeconds = seconds;
+	}
+
+	private static MovementPattern getMovementPatternForType(EnemyType type, Vec2D screenSize, Vec2D enemySize) {
+		switch (type) {
+			case STRAIGHT_NONE:
+			case STRAIGHT_SHOOT:
+				return MovementPattern.newStraight();
+			case WAVE_NONE:
+				return MovementPattern.newWave(1f, 3f);
+			case SIDE_SHOOT:
+				return MovementPattern.newSideToSide(screenSize, enemySize);
+			default:
+				return MovementPattern.newStraight();
+		}
+	}
+	
+	private static AttackPattern getAttackPatternForType(EnemyType type) {
+		switch (type) {
+			case STRAIGHT_SHOOT:
+				return AttackPattern.newSimpleShoot(2.0f);
+			case SIDE_SHOOT:
+				return AttackPattern.newSimpleShoot(1.5f);
+			case STRAIGHT_NONE:
+			case WAVE_NONE:
+			default:
+				return AttackPattern.newNoAttack();
+		}
+	}
+	
+	private static int getLifeForType(EnemyType type) {
+		switch (type) {
+			case STRAIGHT_SHOOT:
+				return 2;
+			case SIDE_SHOOT:
+				return 3;
+			case STRAIGHT_NONE:
+			case WAVE_NONE:
+			default:
+				return 1;
+		}
 	}
 }
---
+++ src/game/entities/patterns/AttackPattern.java
@@ -0,0 +1,57 @@
+package game.entities.patterns;
+
+import game.entities.Bullet;
+import game.entities.Enemy;
+import game.utils.Vec2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public abstract class AttackPattern {
+	public float time = 0;
+
+	public abstract ArrayList<Bullet> attack(Enemy enemy, BufferedImage bulletSprite, float delta);
+	
+	private static class NoAttack extends AttackPattern {
+		@Override
+		public ArrayList<Bullet> attack(Enemy enemy, BufferedImage bulletSprite, float delta) {
+			return new ArrayList<>(); 
+		}
+	}
+	public static AttackPattern newNoAttack() {
+		return new NoAttack();
+	}
+	
+	private static class SimpleShoot extends AttackPattern {
+		private final float cooldown;
+		private float timer = 0;
+		
+		public SimpleShoot(float cooldown) {
+			this.cooldown = cooldown;
+		}
+		
+		@Override
+		public ArrayList<Bullet> attack(Enemy enemy, BufferedImage img, float delta) {
+			ArrayList<Bullet> bullets = new ArrayList<>();
+			
+			timer += delta;
+			if (timer >= cooldown) {
+				timer = 0;
+				
+				Vec2D pos = new Vec2D(
+					enemy.getCenter().x - 20,
+					enemy.collisionShape.y + enemy.collisionShape.height
+				);
+				
+				Bullet bullet = Bullet.newDefaultBullet(pos, img);
+				bullet.movementDirection.y = -1;
+				bullet.spriteDirection.y = -1;
+				bullets.add(bullet);
+			}
+			
+			return bullets;
+		}
+	}
+	public static AttackPattern newSimpleShoot(float cooldown) {
+		return new SimpleShoot(cooldown);
+	}
+}
